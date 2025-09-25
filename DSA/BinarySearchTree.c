a6445ec97f0937b44ea1d17996387b413bfd5da9
Anshumaan Soni <102132714+TETRAWasTaken@users.noreply.github.com>
2025-09-25 05:00:25 +0000

    DSA BST 25/09/25 -1

--- DSA/BinarySearchTree.c
+++ DSA/BinarySearchTree.c
@@ -1,5 +1,6 @@
 // Created by Anshumaan Soni on 22/09/2025
 
+// Imports
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,6 +19,19 @@ Node* createNode(int val){
     newNode->right = NULL;
 
     return newNode;
+}
+
+// Searching a Node in the tree
+Node* search(Node *root, int val){
+    if(root==NULL || root->data==val) {
+        return root;
+    }
+    if(root->data<val) {
+        return search(root->right, val);
+    }
+    else {
+        return search(root->left, val);
+    }
 }
 
 // Inserting a Node in the tree
@@ -53,6 +67,24 @@ void inorder(Node *root){
     inorder(root->right);
 }
 
+// Postorder Traversal of the Tree
+void postorder(Node *root){
+    if(root==NULL) return;
+
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ",root->data);
+}
+
+// Preorder Traversal of the Tree
+void preorder(Node *root){
+    if(root==NULL) return;
+    
+    printf("%d ",root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
 // Main Function
 int main(){
     Node *trees[5];
@@ -64,14 +96,17 @@ int main(){
         printf("1 - Create a new Tree \n");
         printf("2 - Insert into Tree \n");
         printf("3 - Print Inorder Traversal of Tree \n");
-        printf("4 - Exit \n");
+        printf("4 - Print Preorder Traversal of Tree \n");
+        printf("5 - Print Postorder Traversal of Tree \n");
+        printf("6 - Search a value in the Tree \n");
+        printf("9 - Exit \n");
         printf("Enter your choice : ");
         scanf("%d",&select);
 
         // Creating a new Tree
         if(select==1){
             trees[treeindex]=NULL;
-            printf("Tree %d has been created. It is currently empty.\n",treeindex);
+            printf("---- Tree %d has been created. It is currently empty.\n\n",treeindex);
             treeindex++;
             n++;
         }
@@ -105,20 +140,70 @@ int main(){
                 printf("Invalid Tree ID.\n");
                 continue;
             }
-            printf("Inorder Traversal of Tree %d : ",treein);
+            printf("---- Inorder Traversal of Tree %d : ",treein);
             inorder(trees[treein]);
-            printf("\n");
+            printf("\n\n");
+            n++;
+        }
+
+        // Preorder Traversal of a Tree
+        else if(select==4){
+            int treein;
+            printf("Enter the ID of the Tree : ");
+            scanf("%d",&treein);
+            if(treein<0 || treein>=treeindex){
+                printf("Invalid Tree ID.\n");
+                continue;
+            }
+            printf("---- Preorder Traversal of Tree %d : ",treein);
+            preorder(trees[treein]);
+            printf("\n\n");
+            n++;
+        }
+
+        // Postorder Traversal of a Tree
+        else if(select==5){
+            int treein;
+            printf("Enter the ID of the Tree : ");
+            scanf("%d",&treein);
+            if(treein<0 || treein>=treeindex){
+                printf("Invalid Tree ID.\n");
+                continue;
+            }
+            printf("---- Postorder Traversal of Tree %d : ",treein);
+            postorder(trees[treein]);
+            printf("\n\n");
+            n++;
+        }
+
+        // Searching a value in the Tree
+        else if(select==6){
+            int val, treein;
+            printf("Enter the ID of the Tree : ");
+            scanf("%d",&treein);
+            if(treein<0 || treein>=treeindex){
+                printf("Invalid Tree ID.\n");
+                continue;
+            }
+            printf("Enter the value to be searched : ");
+            scanf("%d",&val);
+            Node *res = search(trees[treein], val);
+            if(res==NULL){
+                printf("---- Value %d not found in Tree %d.\n\n", val, treein);
+            }
+            else{
+                printf("---- Value %d found in Tree %d.\n\n", val, treein);
+            }
             n++;
         }
 
         // Exiting the Program
-        else if(select==4){1
-            
+        else if(select==9){
             break;
         }
 
         else{
-            printf("Invalid Choice.\n");
+            printf("---- Invalid Choice.\n\n");
             n++;
         }
     }
